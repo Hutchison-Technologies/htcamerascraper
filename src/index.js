791f55590f1949118163c72abd9265baa3bd3d1c
e.maguire@outlook.com <zl3Mzqhl8sey>
2020-09-04 16:07:07 +0100

    Added batch mode

--- src/index.js
+++ src/index.js
@@ -18,23 +18,27 @@ async function mainLogic(){
     let startDateTime = moment(config.startYear + "-" + config.startMonth + "-" + config.startDay + "T" + config.startHour)
 
     let endDateTime = moment(config.endYear + "-" + config.endMonth + "-" + config.endDay + "T" + config.endHour)
-    let finishedRetrieval = false
-    do {
-        if(startDateTime.isSameOrAfter(endDateTime)){
-            finishedRetrieval = true;
-        }else{
-            getImageUrl(config.apiKey, startDateTime.toISOString(), resultsDir)
-            if(startDateTime.hour >= parseInt(config.endHour)){
-                startDateTime.add(1, 'days');
-                startDateTime.hour(parseInt(config.startHour));
-            }else{
-                startDateTime.add( config.imageRetrievalInterval, "seconds")
+    console.log("Calculating timestamps")
+    const allTimestamps = getAllTimes(startDateTime, endDateTime);
+    let batchindex = 0;
+    let batchValue = 100;
+    let batchImages = []
+    for (let i = 0; i < allTimestamps.length; i++) {
+        const allTimestamp = allTimestamps[i];
+        await getImageUrl(config.apiKey, allTimestamp, resultsDir, batchImages)
+        if(batchindex >= batchValue || i === allTimestamps.length - 1){
+            await new Promise(resolve => setTimeout(resolve, 6000));
+            for (const pictureUrl of batchImages) {
+                await getImageFromUrl(pictureUrl.url, pictureUrl.timestamp, resultsDir)
             }
+            batchindex = 0
+            batchImages = []
+            // await new Promise(resolve => setTimeout(resolve, 5000));
+        }else{
+            batchindex++;
         }
-        await new Promise(resolve => setTimeout(resolve, 200));
+        // await new Promise(resolve => setTimeout(resolve, 3500));
     }
-    while (!finishedRetrieval)
-
     try {
         const { parse } = require('json2csv');
         const csv = parse(pictureUrls);
@@ -47,19 +51,36 @@ async function mainLogic(){
     } catch (err) {
         console.error(err);
     }
-    await new Promise(resolve => setTimeout(resolve, 6000));
-    for (const pictureUrl of pictureUrls) {
-        await getImageFromUrl(pictureUrl.url, pictureUrl.timestamp, resultsDir)
-
-    }
     console.log("Finished at " + moment().toISOString())
 }
 
+function getAllTimes(timestampStart, timestampEnd){
+    let currentTimestamp = timestampStart
+    let allTimes = []
+    allTimes.push(currentTimestamp.toISOString())
+    let finishedRetrieval = false
+    do {
+        if(timestampStart.isSameOrAfter(timestampEnd)){
+            finishedRetrieval = true;
+        }else{
+            if(timestampStart.hour >= parseInt(config.endHour)){
+                currentTimestamp.add(1, 'days');
+                currentTimestamp.hour(parseInt(config.startHour));
+                allTimes.push(currentTimestamp.toISOString())
+            }else{
+                currentTimestamp.add( config.imageRetrievalInterval, "seconds")
+                allTimes.push(currentTimestamp.toISOString())
+            }
+        }
+    }
+    while (!finishedRetrieval)
+    return allTimes;
+}
 
-async function getImageUrl(apiKey, timestamp, fileDestination){
-    var axios = require('axios');
-    const data = JSON.stringify({"timestamp": timestamp});
-    var config = {
+async function getImageUrl(apiKey, timestamp, fileDestination, batchImages){
+    const axios = require('axios');
+    let data = JSON.stringify({"timestamp": timestamp});
+    let config = {
         method: 'post',
         url: 'https://api.meraki.com/api/v0/networks/N_715509390798488988/cameras/Q2TV-QQV8-3ESF/snapshot',
         headers: {
@@ -67,16 +88,17 @@ async function getImageUrl(apiKey, timestamp, fileDestination){
             'Content-Type': 'application/json',
             'X-Cisco-Meraki-API-Key': apiKey
         },
-        data : data
+        data: data
     };
 
     await axios(config)
         .then(async (response) => {
             if(response.status === 202) {
-                console.log(".");
+                // console.log("Got url for " + timestamp);
                 const responseBody = response.data
                 // await new Promise(resolve => setTimeout(resolve, 6000));
                 pictureUrls.push({timestamp: timestamp, url: responseBody.url})
+                batchImages.push({timestamp: timestamp, url: responseBody.url})
                 // await getImageFromUrl(responseBody.url, timestamp, fileDestination)
             }else if(response.status === 429){
                 await new Promise(resolve => setTimeout(resolve, 3000));
@@ -97,8 +119,11 @@ async function getImageFromUrl(imageUrl, timestamp, fileDestination){
 
     await https.get(imageUrl, async function(response) {
         if (response.statusCode === 200){
+            console.log("Saving Image for " + timestamp)
+            console.log(response.statusMessage)
             const file = fs.createWriteStream(fileDestination + "/file_" + timestamp+".jpg");
             response.pipe(file);
+            await new Promise(resolve => setTimeout(resolve, 1000));
         }else if(response.statusCode === 404){
             await new Promise(resolve => setTimeout(resolve, 1000));
             console.log("Issue with image retrieval for " + timestamp)
